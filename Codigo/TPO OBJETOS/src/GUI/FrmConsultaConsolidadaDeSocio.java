911348d870e22eb945ae50aa28e0b18d8e0d3d12
Rodrigo <rodrigo.alcorta@yalochat.com>
2020-11-18 00:30:07 -0300

    ADD: Table of operation's consult

--- Codigo/TPO OBJETOS/src/GUI/FrmConsultaConsolidadaDeSocio.java
+++ Codigo/TPO OBJETOS/src/GUI/FrmConsultaConsolidadaDeSocio.java
@@ -11,6 +11,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class FrmConsultaConsolidadaDeSocio extends JDialog {
     private FrmConsultaConsolidadaDeSocio self;
@@ -29,10 +31,12 @@ public class FrmConsultaConsolidadaDeSocio extends JDialog {
 
         this.setLocationRelativeTo(null);
 
-        model.addColumn("CUIT");
+        model.addColumn("ID");
+        model.addColumn("Tipo");
+        model.addColumn("Importe");
 
         for (Operacion op : operaciones) {
-            model.addRow(new Object[]{op.getCuitCheque()});
+            model.addRow(new Object[]{op.getCuitCheque(), op.getTipoDeOperacion(), op.getImporte()});
         }
 
         model.fireTableDataChanged();
